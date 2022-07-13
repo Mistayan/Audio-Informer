a756290c2d508079e14d0ea23d1228b9338b8797
Mistayan <helixmastaz@gmail.com>
2022-07-13 17:24:56 +0200

    moved async_get out of class AsyncRequest now only have __init__ and get methods.     init : starts an async_get in background's loops     get : wait for async_get to end and return results gathered
    Enhanced test_json to ensure we collect every uuid required with valid formatting

--- src/main/lib/async_testings.py
+++ src/main/lib/async_testings.py
@@ -5,54 +5,53 @@ Project: Learning-Python
 
 import asyncio
 import aiohttp
+import parse
+import re
+
+
+async def async_get(url, query):
+    try:
+        root = url.split('/')[2]
+        if len(root.split('.')) != 2:
+            raise IndexError
+    except IndexError:
+        raise AssertionError(f"url format not valid")
+    async with aiohttp.ClientSession().get(url + query) as response:  # Start a new session for current thread
+        if response.host != root:
+            raise f"{response.host} not {root}"
+        match response.content_type:
+            case 'application/json':
+                return await response.json()
+            case 'text/html':
+                return await response.content.read()
+            case _:
+                return await response.content.read()
+    return  # session or get failed. Unlikely... but may happen
 
 
 class AsyncRequest:
-    _url: str
-    query: str
 
     def __init__(self, url: str, query: str):
-        try:
-            self.root = url.split('/')[2]
-            if len(self.root.split('.')) != 2:
-                raise IndexError
-        except IndexError:
-            raise AssertionError(f"url format not valid")
-        self._url = url
-        self.query = query
-        self.__loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(self.__loop)
-        self.future = self.__prepare_request()
-
-    async def __prepare_request(self):
-        async def async_request(__session):
-            async with __session.get(self._url + self.query) as response:
-                if response.host != self.root:
-                    raise f"{response.host} not {self.root}"
-                match response.content_type:
-                    case 'application/json':
-                        return await response.json()
-                    case 'text/html':
-                        return await response.content.read()
-                    case _:
-                        return await response.content
-
-        async with aiohttp.ClientSession() as session:  # Start a new session for current thread
-            return await asyncio.gather(async_request(session))  # Do async_request
+        self.__loop = asyncio.get_event_loop()
+        self.future = asyncio.gather(async_get(url, query))
 
     def get(self):
-        return self.__loop.run_until_complete(self.future)
+        return self.__loop.run_until_complete(self.future)[0]
 
 
-def test_json():
+def test_json(n: int = 100):
     import time
     print("-_" * 7 + " Async : " + "-_" * 7)
     start = time.perf_counter()
-    prepare = AsyncRequest("https://httpbin.org/", "uuid")
-    results = prepare.get()
-    exec_time = time.perf_counter() - start
-    print(f"nb_of results: {len(results)} \tin: {exec_time:0.3f}s\n")
-    print(results)
+    prepares = [AsyncRequest("https://httpbin.org/", "uuid") for _ in range(n)]
+    # simulate program running
+    # time.sleep(2)
+    results = [prepare.get()['uuid'] for prepare in prepares]
+
+    assert time.perf_counter() - start < 1.1  # If timer goes high, may need optimisations
+    for elem in results:
+        assert len(elem) is 36  # check if every uuid is valid
+        assert re.fullmatch(r"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}", elem) is not None
 
 
 def test_mb_search():
@@ -70,18 +69,17 @@ def test_mb_search():
     print("-_" * 7 + " Async : " + "-_" * 7)
     start = time.perf_counter()
     async_list = [AsyncRequest(site, query) for query in queries]
-    print(f"async sent in :{time.perf_counter() - start:0.3f}s\n")
+    print(f"async sent in :{time.perf_counter() - start:0.5f}s\n")
     results = [job.get() for job in async_list]
-    print(f"nb_of results: {len(results)} \tin :{time.perf_counter() - start:0.3f}s\n")
-    print(results)
+    print(f"nb_of results: {len(results)} \tin :{time.perf_counter() - start:0.5f}s\n")
+    for elem in results:
+        search = parse.search("/release/{0}\"", str(elem))
+        result = search[0] if search else search
+        if type(result) is str:
+            result = result.split('/')[0] if result.find('/') else result  # Avoid /cover-art in some cases
+        print(result)
 
 
 if __name__ == '__main__':
-    import time
-
-    # test_json()
-    try:
-        test_mb_search()
-    except RuntimeError:
-        pass
-    # prepare2 = AsyncRequest("https://musicbrainz.org/search?query=", "\"{0}\"+AND+{1}&type=release&limit=1")
+    print(AsyncRequest("bing.com/?q=", "top+10+hacks+reasons").get()[0])
+    pass
