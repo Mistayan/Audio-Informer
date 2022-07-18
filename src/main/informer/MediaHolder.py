d9cc568ce1d90bbc6e3fe46824ace3fa51fdb4aa
Mistayan <helixmastaz@gmail.com>
2022-07-18 19:19:56 +0200

    enhanced repr, combining every search available for media get_shazam now uses AsyncRequest holder

--- src/main/informer/MediaHolder.py
+++ src/main/informer/MediaHolder.py
@@ -2,22 +2,19 @@
 Created by: Mistayan
 Project: Audio-Informer
 """
-import asyncio
-import os
-
 import logging
+import os
 import re
 import time
-from typing import Final
 
 import mutagen
+
 from conf import api as conf
-
-# Test's confs
-# from parsers.musicbrainz import musicbrainz_api
-
-logger: Final = logging.getLogger(__name__)
-logger.level = 3
+from lib.my_async import AsyncRequest
+from lib.utils import get_file_name
+from parsers.musicbrainz import musicbrainz_api
+from parsers.shazam import shazam_it, get_shazam_lyrics
+logger = logging.getLogger(__name__)
 
 
 class MediaHolder:
@@ -25,51 +22,73 @@ class MediaHolder:
     Holds a valid file, with path, name, media details and its fingerprint
 
     """
-    intel: mutagen.FileType = None
-    shazam = None
 
-    def __init__(self, path: str, **kwargs):
-        self.path = re.sub(r"\\+", "/", path)
-        if not self.valid:
-            self.logger.warning(f"Cannot open {self.path} => not a valid file")
+    def __init__(self, path: str, callback=None, **kwargs):
+        if not self.is_valid(path):
+            logger.error(f"{path} => not a valid file")
             return
-        logger.debug(f"Storing {path} as Media")
-        self.name = self.path.split("/")[-1]
         try:
-            logger.debug(f"Mutagen on Media")
-            self.intel = self.mutagen_load()
-        except MutagenError as err:
-            logger.error(err)
-            raise ChildProcessError(err)
+            self.path = re.sub(r"\\+", "/", path)
+            self.name = get_file_name(self.path)
+        except IndexError:
+            logger.error(f"{self.path} => not a valid path")
+            return
+        self.intel = None
+        self.future = None
+        self.shazam = None
+        self.musicbrainz = None
+        self.additional = None
+        self.lyrics = None
+
+        self.shazam = AsyncRequest(shazam_it, self.path)
+        self.shazam.add_done_callback(callback)
+        self.get_mutagen()
+        # TODO: ensure MPEG 'TABL', 'TIT2', 'TPE1', TCON to be loaded as classic intel
+        # TODO: ensure FLAC 'TITLE', 'ALBUMARTIST' > 'ARTIST', 'DATE', 'ALBUM', 'GENRE', 'TRACKNUMUBER', 'TOTALTRACKS'
+
+        if not (self.intel or self.shazam):
+            logger.warning(f"failed to open : {self.name} ")
+            return
+        logger.info(f"Storing {self.name} as Media")
 
     # ---------------------------------- Logic methods ---------------------------------- #
-
     @staticmethod
-    def is_valid(path) -> bool:
-        """returns True if file exists and has a valid file extension"""
+    def is_valid(path: str) -> bool:
+        """returns True if file can be theoretically loaded as media"""
         return os.path.isfile(path) and str(path).upper().endswith(conf.VALID_EXTENSIONS)
 
     # ---------------------------------- Outer methods ---------------------------------- #
-    def mutagen_load(self) -> mutagen.FileType | None:
-        try:
-            logger.info(f"Opening with mutagen...{self.path} ")
-            return mutagen.File(self.path)
-        except MutagenError as err:
-            logger.warning(f"failed to open: {err}")
-            return None
+    # -------------------------- methods that should be used  --------------------------- #
+    def get_mutagen(self) -> mutagen.FileType | None:
+        if not self.intel:
+            try:
+                self.intel = mutagen.File(self.path, easy=True)
+                if self.intel:
+                    logger.info(f"opened successfully : {self.name} ")
+                    logger.debug(f"results: {self.intel}")
+            except mutagen.MutagenError as err:
+                logger.warning(f"failed to open: {err}")
+                return
+        return self.intel
 
-    def calc_fingerprint(self):
-        async def shazam_it():
-            logger.debug(f"Shazaming : {self.path}")
-            return await asyncio.gather(Shazam().recognize_song(self.path))
-
-        loop = asyncio.new_event_loop()
-        return loop.run_until_complete(shazam_it())
-
+    def get_shazam(self) -> dict | None:
+        if isinstance(self.shazam, AsyncRequest):
+            self.shazam = self.shazam.get()
         return self.shazam
+
+    def get_musicbrainz(self):
+        if not self.musicbrainz:
+            self.musicbrainz = musicbrainz_api(self.get_shazam() or self.get_mutagen())
+        return self.musicbrainz
+
+    def get_lyrics(self):
+        if not self.lyrics:
+            self.lyrics = get_shazam_lyrics(self.get_shazam()['lyrics_url']) if self.shazam else None
+        return  self.lyrics
 
     def update_id3(self):  # from https://programtalk.com/python-examples/mutagen.File/
         """Update ID3 tags in outfile"""
+        # TODO: evaluate gathered intel to store in-file
         audio = mutagen.File(self.path, easy=True)
         audio.add_tags()
         audio["title"] = self.intel["title"]
@@ -85,32 +104,39 @@ class MediaHolder:
         audio.save()
 
     # ---------------------------------- Magic functions ---------------------------------- #
-
     def __str__(self):
         return self.name
 
-    def __await__(self):
-        # if asyncio.coroutines.iscoroutine(self.shazam):
-        #     asyncio.coroutines.
-        pass
-
     def __repr__(self):
-        return [self.intel.__repr__(), self.shazam] \
-            if self.intel and self.shazam \
-            else self.intel if self.intel \
-            else ''
+        return {
+            "name": self.name,
+            "mutagen": self.intel,
+            "shazam": self.get_shazam(),
+            "lyrics": self.get_lyrics(),
+            "musicbrainz":  self.get_musicbrainz(),
+            # TODO: Implement more researches for :
+            # better genders analysis
+            #
+        }
 
 
 # ---------------------------------- Test Class ---------------------------------- #
-if __name__ == "__main__":
-    from pprint import pprint
-
-    file = "C:/Users/GAMER/Desktop/python-learnin/Audio-Informer/tests/test_dir/second_rec_test/08. Crescent.flac"
+def test_class(file="../../tests/test_dir/music_without_tags.mp3"):
+    import pprint
+    import time
+    print()
     s = time.perf_counter()
-    print("Starting ")
     media = MediaHolder(file)
-    print(media.intel)
-    # print(musicbrainz_api(media.intel))
-    pprint(media, indent=4)
+    if media:
+        pprint.pprint(media.__repr__())
     elapsed = time.perf_counter() - s
     print(f"{__file__} executed in {elapsed:0.2f} seconds.")
+
+
+def test_rubbish():
+    media = MediaHolder("../../tests/test_dir/dumb_folder/15.mp4").__repr__()
+    for field in media:
+        assert media[field] is None
+
+if __name__ == "__main__":
+    pass
---
+++ src/main/parsers/shazam.py
@@ -0,0 +1,89 @@
+"""
+Created by: Mistayan
+Project: Learning-Python
+Creation date: 07/15/22
+"""
+import parse
+import logging
+
+import pydub.exceptions
+from shazamio import Shazam
+from lib.my_async import AsyncRequest, get_content
+from lib.utils import save_thumbnail, get_file_name
+
+log = logging.getLogger(__name__)
+
+
+async def shazam_it(file) -> dict | None:
+    """
+    async shazam the file provided
+    gather any useful intel and sends back a dict
+    """
+    name = get_file_name(file)
+    try:
+        log.debug(f"trying to shazam : {name}")
+        shazam = await Shazam().recognize_song(file)
+    except pydub.exceptions.PydubException:
+        shazam = None
+    if not shazam:
+        log.info(f"shazam failed on : {name}")
+        return
+    try:
+        track = shazam['track']
+    except KeyError:  # No results
+        return
+    log.debug(f"shazam succeeded on : {name}")
+    sections = track['sections']
+    meta = sections[0]['metadata']
+    # TODO: Test with different songs, to avoid errors (so far so good, but need more testings)
+    return {"title": track['title'],
+            "artist": track['subtitle'],
+            "genres": track['genres'],
+            "album": meta[0]['text'],
+            "release_date": meta[2]['text'],
+            "record_label": meta[1]['text'],
+            "youtube_redirect": sections[2]['youtubeurl'] if len(sections) >= 3 and 'youtubeurl' in sections[2] else '',
+            "similar_songs_url": sections[3]['url'] if len(sections) >= 4 and 'url' in sections[3] else '',
+            "thumb_url": track['images']['coverart'] if 'images' in track and 'coverart' in track['images'] else '',
+            "lyrics_url": sections[1]['url'] if len(sections) >= 2 and 'url' in sections[1] else '',
+            "lyrics": get_shazam_lyrics(sections[1]['url']) if len(sections) >= 2 and 'url' in sections[1] else '',
+            }
+    # thu = AsyncRequest(intel['thumb_url'])
+    # save_thumbnail(f"{intel['album']}", thu.get())
+
+
+def get_shazam_lyrics(url: str, no_offset=True) -> dict | None:
+    """
+    get lyrics from shazam url
+    returns a dict: { 'text': ['offset': timestamp, text] | [ text ] }
+    """
+    if not url or not isinstance(url, str):
+        return
+    lyrics_infos = AsyncRequest(get_content, url).get()  # expect json
+
+    if not lyrics_infos:
+        return
+    pattern = parse.Parser("): {0}\n")
+    wr_infos = pattern.search(lyrics_infos['footer'])
+    if wr_infos:
+        if wr_infos[0].find(","):
+            writers = wr_infos[0].split(", ")
+        else:
+            writers = wr_infos[0]
+    else:
+        writers = ''
+
+    return {"writers": writers,
+            "text":
+                [offset['text'] for offset in lyrics_infos['syncedtext']]
+                if no_offset else lyrics_infos['syncedtext']}
+
+
+if __name__ == "__main__":
+    import pprint
+
+    file_path = "../../tests/test_dir/music_without_tags.mp3"
+    result = AsyncRequest(shazam_it, file_path).get()
+    pprint.pprint(result)
+    # pprint.pprint(get_lyrics(result['lyrics_url']), compact=False, indent=2)
+
