5fab08df11f65a2e5f1b9d1cb951705495802a4e
Mistayan <helixmastaz@gmail.com>
2022-07-27 15:08:30 +0200

    bugs resolutions

--- src/main/lib/my_async.py
+++ src/main/lib/my_async.py
@@ -7,7 +7,9 @@ import asyncio
 import logging
 
 import aiohttp
+import parse
 import yarl
+import brotli  # Useful for br pages (more and more common on the web)
 import nest_asyncio
 
 nest_asyncio.apply()
@@ -26,21 +28,27 @@ async def get_content(url: str, query: str = ''):
         return
     target = yarl.URL(url + query)  # proper request formatting
     log.info(f"request : {target}")
-    async with aiohttp.ClientSession() as session:  # Start a new session for current thread
-        async with session.get(target) as response:  # Get session's content
-            if response.host != target.host:
-                raise f"{response.host} not {target.host}"
-            if not response.status == 200:
-                ret = response
-            else:
-                match response.content_type:
-                    case 'application/json':
-                        ret = await response.json()
-                    case 'text/html':
-                        ret = await response.read()
-                    case _:
-                        ret = await response.content.read()
-    log.debug(f"result for {target.human_repr()}: {ret if not isinstance(ret, bytes) and len(ret) < 500 else 'bytes.'}")
+    try:
+        async with aiohttp.ClientSession() as session:  # Start a new session for current thread
+            async with session.get(target) as response:  # Get session's content
+                # if response.host != target.host and "www." + response.host != target.host:
+                #     raise ValueError(f"{response.host} not {target.host}")
+                if not response.status == 200:
+                    return
+                else:
+                    log.debug(response.content_type)
+                    match response.content_type:
+                        case 'application/json':
+                            ret: dict = await response.json()
+                        case 'text/html':
+                            ret: str = await response.text()
+                        case _:
+                            ret = await response.content.read()
+        if response.status == 200:
+            log.debug(f"result for {target.human_repr()}: {ret if not isinstance(ret, bytes) else 'bytes.'}")
+    except aiohttp.ClientConnectorError:
+        log.debug(f"Connection not established with {target.human_repr()}")
+        ret = None
     return ret
 
 
@@ -76,12 +84,12 @@ class AsyncRequest:
         self.future = None
         # sanity checks
         try:
-            self.__loop = asyncio.get_event_loop()
-            self.future = asyncio.Task(func(*args))  # initiate request as background task
+            self.__loop = asyncio.get_event_loop_policy().get_event_loop()
+            self.future = asyncio.Task(func(*args), loop=self.__loop)  # initiate request as background task
             self.future.add_done_callback(self._set_result)
             asyncio.set_event_loop(self.__loop)
             self.result = None
-        except RuntimeError:  # multithread / multiprocess context
+        except RuntimeError or RuntimeWarning:  # multithread / multiprocess context
             self.result = asyncio.run(func(*args))
         self.end = time.perf_counter()
 
@@ -102,18 +110,21 @@ class AsyncRequest:
 
     def get(self):
         """
-        If internal callback is received (Thread raised an Error, or ended), skip freeze
-        Will freeze calling process until result is available.
-        when done, stop current loop after ensuring _Future
+        If internal callback is received (Thread raised an Error, or ended), skip freeze \n
+        Will freeze calling process until result is available. \n
+        When done, stop current loop after ensuring _Future \n
         Release newly created loop (if not main thread's loop)
         """
-        if self.future and not self.future.done():
-            self.__loop = self.future.get_loop()
-            if not self.future.get_loop().is_running():
-                self.__loop.run_until_complete(self.future)
-            if not self.result:
-                self.result = self.future.result()
-                self.future = None
+        try:
+            if self.future and (not self.future.done() or not self.result):
+                loop = self.future.get_loop()
+                if not loop.is_running():
+                    loop.run_until_complete(self.future)
+                if not self.result:
+                    self.result = self.future.result()
+                    self.future = None
+        except:  # Either loop is done and closed or could not process requested datas.
+            pass
         return self.result
 
     def close(self, cb=None):
@@ -138,7 +149,7 @@ class AsyncRequest:
 # _______________________ TESTS ______________________ #
 # ____________________________________________________ #
 
-def test_json(n: int = 55):
+def test_json(n: int = 10):
     import re
     import time
     from uuid import UUID
@@ -146,8 +157,8 @@ def test_json(n: int = 55):
     s = time.time()
     prepares = [AsyncRequest(get_content, "https://httpbin.org/", "uuid") for _ in range(n)]
     results = [prepare.get() for prepare in prepares]
-    # assert time.perf_counter() - start < n / 6  # If timer goes high, may need optimisations
-    # print("speed test OK")
+    assert time.perf_counter() - s < n / 5  # If timer goes high, may need optimisations
+    print("speed test OK")
 
     assert len(results) == n and None not in results
     for elem in results:
@@ -165,14 +176,14 @@ def test_mb_search():
 
     print()
     site = "https://musicbrainz.org/search?query="
-    sub = "\"{0}\"+AND+\"{1}\"&type=release&limit=1"
-    queries = [sub.format("Lady Gaga", "Shallow"),
-               sub.format("Ratatat", "Loud Pipes"),
-               sub.format("Marina", "Pink"),
-               sub.format("Miley", "Wrecking "),
-               sub.format("fleebag", "YUNGBLUD"),
-               sub.format("Retour vers le futur", ""),
-               sub.format("", "Who Wants to Live Forever"),
+    query = "\"{0}\"+AND+\"{1}\"&type=release&limit=1"
+    queries = [query.format("Lady Gaga", "Shallow"),
+               query.format("Ratatat", "Loud Pipes"),
+               query.format("Marina", "Pink"),
+               query.format("Miley", "Wrecking "),
+               query.format("fleebag", "YUNGBLUD"),
+               query.format("Retour vers le futur", ""),
+               query.format("", "Who Wants to Live Forever"),  # Sadly returns None
                "BadRequest"]
     print("-_" * 7 + " Async : " + "-_" * 7)
     start = time.perf_counter()
@@ -191,7 +202,13 @@ def test_mb_search():
 
 if __name__ == '__main__':
     test_json()
-    test_mb_search()  # TODO: real tests, no prints
-    req = AsyncRequest("https://www.bing.com/?q=", "top+10+hacks+reasons")
-    print(req.get())
-    pass
+    test_mb_search()
+
+    req = AsyncRequest(get_content, "https://www.bing.com/?", "q=top+10+hacks+reasons")
+    req.get()
+    if req.get():
+        href_finder = parse.Parser("href=\"{url}\"")
+        links = href_finder.findall(req.get())
+        for link in links:
+            if link["url"][0] == 'h':
+                print(link['url'])
