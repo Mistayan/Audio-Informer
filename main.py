84d2eccab5410e51a7eac8fec3a3fb40482a1f70
Mistayan <helixmastaz@gmail.com>
2022-07-28 15:40:43 +0200

    reworked parse_main_args for more comprehensible output renamed load_files to find_files added run_one_process and run_multi_process (one_process is there just for comparison of time gained)

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import logging
 import os
-import pprint
 import re
 import sys
 import time
+from multiprocessing import Pool
 
 from conf import api as conf
 from informer.MediaHolder import MediaHolder
@@ -19,7 +19,16 @@ def print_help(msg: str = None) -> None:
     exit(-1)
 
 
-def load_files(current_dir: str) -> list:
+def run_multi_process(files):
+    with Pool(processes=conf.MAX_WORKERS) as pool:
+        return pool.map_async(MediaHolder, files).get()
+
+
+def run_one_process(files):
+    return [MediaHolder(file) for file in files]
+
+
+def find_files(current_dir: str) -> list:
     """
     :param current_dir: directory to scan for valid audio files
     :return list:
@@ -30,33 +39,33 @@ def load_files(current_dir: str) -> list:
             abs_file_path = os.path.join(root, file)
             # File exists and has valid extension
             if MediaHolder.is_valid(abs_file_path):
-                print(abs_file_path)
-                ret.append(MediaHolder(abs_file_path))
-
+                ret.append(abs_file_path)
     return ret
 
 
 def parse_main_args(args: list) -> list:
     """
-    :param args: list of program's arguments
-    :return : a list of every valid files found in args, loaded in a MediaHolder
-     File: [valid_file]
-     Path: [[valid_files_in_dir]]
+        :param args: list of program's arguments
+        :return :
+        a list of every valid files found in args.
+        File: [valid_file]
+        Path: [valid_files in arg]
+        multiple args: [valid file(s), valid_files_in_folder(s)]
     """
     if len(args) <= 0:
         print_help()
-
-    path = os.getcwd()  # Current_workdir
     ret = []
+    path = os.getcwd()  # Current_workdir
     for arg in args:
-        abs_path = re.sub(r"\\+", "/", os.path.join(path, arg))
+        # friendly path formatting of an absolute path
+        abs_path = re.sub(r"\\+", "/", arg if os.path.isabs(arg) else os.path.join(path, arg))
         if not os.path.exists(abs_path):
             logger.warning(f"{abs_path}  : does not exists")
-            return ret
-        elif os.path.isdir(abs_path):  # if path is a folder
-            ret = load_files(abs_path)
+            return []
+        elif os.path.isdir(abs_path):
+            return parse_main_args(find_files(abs_path))
         elif MediaHolder.is_valid(abs_path):
-            ret.append(MediaHolder(abs_path))
+            ret.append(abs_path)
     return ret
 
 
@@ -65,13 +74,16 @@ if __name__ == '__main__':
     # else append single file to MediaHolder_list (if compatible)
     s = time.perf_counter()
     if len(sys.argv) >= 2 and re.match(r"", sys.argv[0].split("\\")[-1]):  # python main args
-        medias = parse_main_args(sys.argv[1:])
+        files = parse_main_args(sys.argv[1:])
     else:
         print("no path/media chosen. Executing on src/tests/test_dir")
-        medias = parse_main_args(["src/tests/test_dir/"])
+        files = parse_main_args(["src/tests/test_dir/"])
+    print(files)
+    # medias = run_one_process(files)  # You can compare speed by un-commenting this line
     print(f"files found , loaded, and researches have started in {time.perf_counter() - s:0.5f} seconds.")
+    medias = run_multi_process(files)
     # media: MediaHolder
     for media in medias:
         print(media.__repr__())
-    # TODO: next step, compare found intel and ask user to validate.
+    # TODO: next step, improve musicbrainz api then compare found intel and ask user to validate.
     print(f"{__file__} executed in {time.perf_counter() - s:0.3f} seconds. ({len(medias)} files processed)")
